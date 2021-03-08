4474bf571d687fdd9c62c8f94128df673dd7ee27
JoshuaJHaines <joshjhaines3@gmail.com>
2021-03-08 14:19:56 -0500

    created 'containsUpperCase' and 'containsLowerCase function. tests passed

--- main.js
+++ main.js
@@ -65,12 +65,88 @@ function containsDigit(str) {
 	}
 }
 
-test = containsDigit('35132')
-test
 
-function containsLowerCase() {}
+function containsLowerCase(str) {
+	numLowers = 0;
+	for (i = 0; i < str.length; i++) {
+		if (
+			str[i] === 'a' ||
+			str[i] === 'b' ||
+			str[i] === 'c' ||
+			str[i] === 'd' ||
+			str[i] === 'e' ||
+			str[i] === 'f' ||
+			str[i] === 'g' ||
+			str[i] === 'h' ||
+			str[i] === 'i' ||
+			str[i] === 'j' ||
+			str[i] === 'k' ||
+			str[i] === 'l' ||
+			str[i] === 'm' ||
+			str[i] === 'n' ||
+			str[i] === 'o' ||
+			str[i] === 'p' ||
+			str[i] === 'q' ||
+			str[i] === 'r' ||
+			str[i] === 's' ||
+			str[i] === 't' ||
+			str[i] === 'u' ||
+			str[i] === 'v' ||
+			str[i] === 'w' ||
+			str[i] === 'x' ||
+			str[i] === 'y' ||
+			str[i] === 'z'
+		) {
+			numLowers += 1;
+		}
+	}
+	if (numLowers < 1) {
+		return false;
+	} else {
+		return true;
+	}
+}
 
-function containsUpperCase() {}
+function containsUpperCase(str) {
+	numLowers = 0;
+	for (i = 0; i < str.length; i++) {
+		if (
+			str[i] === 'A' ||
+			str[i] === 'B' ||
+			str[i] === 'C' ||
+			str[i] === 'D' ||
+			str[i] === 'E' ||
+			str[i] === 'F' ||
+			str[i] === 'G' ||
+			str[i] === 'H' ||
+			str[i] === 'I' ||
+			str[i] === 'J' ||
+			str[i] === 'K' ||
+			str[i] === 'L' ||
+			str[i] === 'M' ||
+			str[i] === 'N' ||
+			str[i] === 'O' ||
+			str[i] === 'P' ||
+			str[i] === 'Q' ||
+			str[i] === 'R' ||
+			str[i] === 'S' ||
+			str[i] === 'T' ||
+			str[i] === 'U' ||
+			str[i] === 'V' ||
+			str[i] === 'W' ||
+			str[i] === 'X' ||
+			str[i] === 'Y' ||
+			str[i] === 'Z'
+		) {
+			numLowers += 1;
+		}
+	}
+	if (numLowers < 1) {
+		return false;
+	} else {
+		return true;
+	}
+}
 
 function containsNonAlphanumeric() {}
 
