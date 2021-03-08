c6dd59d8ef2279d70976209ec68e86f923584845
JoshuaJHaines <joshjhaines3@gmail.com>
2021-03-08 13:13:27 -0500

    created 'countWords' function. tests passed

--- main.js
+++ main.js
@@ -1,75 +1,76 @@
 function isEvenlyDivisible(num1, num2) {
-  if (num1 % num2 === 0){
-    return true
-  } else {
-    return false
-  }
+	if (num1 % num2 === 0) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 function halfSquare(num) {
-  return num * num / 2
+	return (num * num) / 2;
 }
 
 function isLong(str) {
-  if (str.length >= 15){
-    return true
-  } else {
-    return false
-  }
+	if (str.length >= 15) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
+//not working, continuing
 function exclaim(str) {
-  if (str.endsWith() !== '!'){
-    return str += '!'
-  } else {
-    return str
-  }
+	if (str.endsWith("!") !== "!") {
+		return (str += "!");
+	}
+	if (str.endsWith("!!") === "!!") {
+		return str.text.slice(0, -1);
+	} else {
+		return str;
+	}
 }
 
-function countWords() {
+function countWords(str) {
+	let wordCount = 1;
+	for (i = 0; i < str.length; i++) {
+		if (str[i] === " ") {
+			wordCount += 1;
+		}
+	}
+	return wordCount;
 }
 
-function containsDigit() {
-}
+function containsDigit() {}
 
-function containsLowerCase() {
-}
+function containsLowerCase() {}
 
-function containsUpperCase() {
-}
+function containsUpperCase() {}
 
-function containsNonAlphanumeric() {
-}
+function containsNonAlphanumeric() {}
 
-function containsSpace() {
-}
+function containsSpace() {}
 
-function digits() {
-}
+function digits() {}
 
-function truncate() {
-}
+function truncate() {}
 
-function isValidPassword() {
-}
+function isValidPassword() {}
 
-function onlyPunchy() {
-}
-
+function onlyPunchy() {}
 
 module.exports = {
-  isEvenlyDivisible,
-  halfSquare,
-  exclaim,
-  isLong,
-  containsDigit,
-  containsLowerCase,
-  containsUpperCase,
-  containsNonAlphanumeric,
-  containsSpace,
-  countWords,
-  digits,
-  truncate,
-  isValidPassword,
-  onlyPunchy,
-}
+	isEvenlyDivisible,
+	halfSquare,
+	exclaim,
+	isLong,
+	containsDigit,
+	containsLowerCase,
+	containsUpperCase,
+	containsNonAlphanumeric,
+	containsSpace,
+	countWords,
+	digits,
+	truncate,
+	isValidPassword,
+	onlyPunchy,
+};
