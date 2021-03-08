e2fb59f421dd33c70d91186cee1d3c05abe60a88
JoshuaJHaines <joshjhaines3@gmail.com>
2021-03-08 14:41:36 -0500

    created 'isValidPassword' function. tests passed

--- main.js
+++ main.js
@@ -44,16 +44,16 @@ function containsDigit(str) {
 	numDigits = 0;
 	for (i = 0; i < str.length; i++) {
 		if (
-			str[i] === '0' ||
-			str[i] === '1' ||
-			str[i] === '2' ||
-			str[i] === '3' ||
-			str[i] === '4' ||
-			str[i] === '5' ||
-			str[i] === '6' ||
-			str[i] === '7' ||
-			str[i] === '8' ||
-			str[i] === '9'
+			str[i] === "0" ||
+			str[i] === "1" ||
+			str[i] === "2" ||
+			str[i] === "3" ||
+			str[i] === "4" ||
+			str[i] === "5" ||
+			str[i] === "6" ||
+			str[i] === "7" ||
+			str[i] === "8" ||
+			str[i] === "9"
 		) {
 			numDigits += 1;
 		}
@@ -65,37 +65,36 @@ function containsDigit(str) {
 	}
 }
 
-
 function containsLowerCase(str) {
 	numLowers = 0;
 	for (i = 0; i < str.length; i++) {
 		if (
-			str[i] === 'a' ||
-			str[i] === 'b' ||
-			str[i] === 'c' ||
-			str[i] === 'd' ||
-			str[i] === 'e' ||
-			str[i] === 'f' ||
-			str[i] === 'g' ||
-			str[i] === 'h' ||
-			str[i] === 'i' ||
-			str[i] === 'j' ||
-			str[i] === 'k' ||
-			str[i] === 'l' ||
-			str[i] === 'm' ||
-			str[i] === 'n' ||
-			str[i] === 'o' ||
-			str[i] === 'p' ||
-			str[i] === 'q' ||
-			str[i] === 'r' ||
-			str[i] === 's' ||
-			str[i] === 't' ||
-			str[i] === 'u' ||
-			str[i] === 'v' ||
-			str[i] === 'w' ||
-			str[i] === 'x' ||
-			str[i] === 'y' ||
-			str[i] === 'z'
+			str[i] === "a" ||
+			str[i] === "b" ||
+			str[i] === "c" ||
+			str[i] === "d" ||
+			str[i] === "e" ||
+			str[i] === "f" ||
+			str[i] === "g" ||
+			str[i] === "h" ||
+			str[i] === "i" ||
+			str[i] === "j" ||
+			str[i] === "k" ||
+			str[i] === "l" ||
+			str[i] === "m" ||
+			str[i] === "n" ||
+			str[i] === "o" ||
+			str[i] === "p" ||
+			str[i] === "q" ||
+			str[i] === "r" ||
+			str[i] === "s" ||
+			str[i] === "t" ||
+			str[i] === "u" ||
+			str[i] === "v" ||
+			str[i] === "w" ||
+			str[i] === "x" ||
+			str[i] === "y" ||
+			str[i] === "z"
 		) {
 			numLowers += 1;
 		}
@@ -111,32 +110,32 @@ function containsUpperCase(str) {
 	numUppers = 0;
 	for (i = 0; i < str.length; i++) {
 		if (
-			str[i] === 'A' ||
-			str[i] === 'B' ||
-			str[i] === 'C' ||
-			str[i] === 'D' ||
-			str[i] === 'E' ||
-			str[i] === 'F' ||
-			str[i] === 'G' ||
-			str[i] === 'H' ||
-			str[i] === 'I' ||
-			str[i] === 'J' ||
-			str[i] === 'K' ||
-			str[i] === 'L' ||
-			str[i] === 'M' ||
-			str[i] === 'N' ||
-			str[i] === 'O' ||
-			str[i] === 'P' ||
-			str[i] === 'Q' ||
-			str[i] === 'R' ||
-			str[i] === 'S' ||
-			str[i] === 'T' ||
-			str[i] === 'U' ||
-			str[i] === 'V' ||
-			str[i] === 'W' ||
-			str[i] === 'X' ||
-			str[i] === 'Y' ||
-			str[i] === 'Z'
+			str[i] === "A" ||
+			str[i] === "B" ||
+			str[i] === "C" ||
+			str[i] === "D" ||
+			str[i] === "E" ||
+			str[i] === "F" ||
+			str[i] === "G" ||
+			str[i] === "H" ||
+			str[i] === "I" ||
+			str[i] === "J" ||
+			str[i] === "K" ||
+			str[i] === "L" ||
+			str[i] === "M" ||
+			str[i] === "N" ||
+			str[i] === "O" ||
+			str[i] === "P" ||
+			str[i] === "Q" ||
+			str[i] === "R" ||
+			str[i] === "S" ||
+			str[i] === "T" ||
+			str[i] === "U" ||
+			str[i] === "V" ||
+			str[i] === "W" ||
+			str[i] === "X" ||
+			str[i] === "Y" ||
+			str[i] === "Z"
 		) {
 			numUppers += 1;
 		}
@@ -152,18 +151,18 @@ function containsNonAlphanumeric(str) {
 	numLowers = 0;
 	for (i = 0; i < str.length; i++) {
 		if (
-			str[i] === ' ' ||
-			str[i] === '?' ||
-			str[i] === '#' ||
-			str[i] === '$' ||
-			str[i] === '%' ||
-			str[i] === '^' ||
-			str[i] === '&' ||
-			str[i] === '@' ||
-			str[i] === '*' ||
-			str[i] === '(' ||
-			str[i] === ')' ||
-			str[i] === '!'
+			str[i] === " " ||
+			str[i] === "?" ||
+			str[i] === "#" ||
+			str[i] === "$" ||
+			str[i] === "%" ||
+			str[i] === "^" ||
+			str[i] === "&" ||
+			str[i] === "@" ||
+			str[i] === "*" ||
+			str[i] === "(" ||
+			str[i] === ")" ||
+			str[i] === "!"
 		) {
 			numLowers += 1;
 		}
@@ -178,9 +177,7 @@ function containsNonAlphanumeric(str) {
 function containsSpace(str) {
 	numSpaces = 0;
 	for (i = 0; i < str.length; i++) {
-		if (
-			str[i] === ' '
-		) {
+		if (str[i] === " ") {
 			numSpaces += 1;
 		}
 	}
@@ -206,10 +203,20 @@ function truncate(str) {
 		return str;
 	}
 }
-{
-}
 
-function isValidPassword() {}
+function isValidPassword(str) {
+	if (
+		containsUpperCase(str) === true &&
+		containsLowerCase(str) === true &&
+		containsDigit(str) === true &&
+		containsNonAlphanumeric(str) === true &&
+		containsSpace(str) === false
+	) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
 function onlyPunchy() {}
 
